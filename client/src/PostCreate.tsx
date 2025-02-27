b95cb14af0d0cde5b42f10910756f0330dc34442
harold montes <haroldjosehd24@gmail.com>
2025-02-27 10:54:03 -0400

    implement post submission form

---
+++ client/src/PostCreate.tsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import {useState} from 'react';
+
+const PostCreate = () => {
+
+const [title, setTitle] = useState('');
+
+const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:4001/posts', title);
+    setTitle('');
+}
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="">title</label>
+                <input 
+                type="text" 
+                className="form-control"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default PostCreate
