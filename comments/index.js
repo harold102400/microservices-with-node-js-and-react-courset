a3c5cbb3c9dec9481228b1ed8c4fd024975e3287
harold montes <hmontes@gruporead.com>
2025-03-18 21:19:29 -0400

    update comment content

--- comments/index.js
+++ comments/index.js
@@ -38,8 +38,35 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log("Event Received: ", req.body.type);
+
+
+  const {type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const {postId, id, status, content} = data;
+
+    ///Todos los comentarios asosciados con el postId
+    const comments = commentsByPostId[postId];
+    //busca el comentario
+    const comment = comments.find(comment => {
+      return comment.id === id
+    });
+    //actualiza el status
+    comment.status = status;
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    });
+  }
+
+
 
   res.send({});
 });
