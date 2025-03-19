f52e3458b6b6ce66de4a8865f877dadcf9617442
harold montes <hmontes@gruporead.com>
2025-03-18 22:25:40 -0400

    render comments by status

--- client/src/CommentList.tsx
+++ client/src/CommentList.tsx
@@ -1,6 +1,6 @@
 import { Comment } from "../src/types/comments";
 
-const CommentList = ({ comments }:{comments: Comment[] }) => {
+const CommentList = ({ comments }: { comments: Comment[] }) => {
   // const [comments, setComments] = useState<Comment[]>([]);
 
   // const fetchData = async () => {
@@ -16,7 +16,24 @@ const CommentList = ({ comments }:{comments: Comment[] }) => {
   // }, []);
 
   const renderedComments = comments?.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+
+    let content = '';
+
+    switch (comment.status) {
+      case 'approved':
+        content = comment.content
+        break;
+      case 'pending':
+        content = 'This comment is awaiting moderation'
+        break;
+      case 'rejected':
+        content = 'This comment has been rejected'
+        break;
+      default:
+        console.log('Content is empty or unrecognized');
+        break;
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
